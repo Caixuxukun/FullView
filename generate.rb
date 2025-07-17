d090ad045c037d7e4e633f704d71928cab1da06b
Caixuxukun <90492691+Caixuxukun@users.noreply.github.com>
2025-07-17 05:00:32 +0000

    name

--- generate.rb
+++ generate.rb
@@ -1,13 +1,13 @@
 require 'xcodeproj'
 
 # 创建一个空工程
-project = Xcodeproj::Project.new('MyApp.xcodeproj')
+project = Xcodeproj::Project.new('FullView.xcodeproj')
 # 添一个 iOS app target
-target = project.new_target(:application, 'MyApp', :ios, '15.0')
+target = project.new_target(:application, 'FullView', :ios, '15.0')
 # 在工程的主 Group 下建一个 Sources 组
 group = project.main_group.new_group('Sources')
 # 把 test.swift 加进 Sources
-file  = group.new_file('test.swift')
+file  = group.new_file('main.swift')
 # 让 target 编译这个文件
 target.add_file_references([file])
 # 保存 .xcodeproj
